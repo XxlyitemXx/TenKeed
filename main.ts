b79b97594837cb75f0e3ded2676f905ad717d3ed
imaonenub <93632222+imaonenub@users.noreply.github.com>
2023-11-02 20:05:33 +0700

    add some changes

--- main.ts
+++ main.ts
@@ -25,18 +25,25 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     }
 })
 function show_title () {
-    basic.showString(serial.readString())
+    basic.showString(SerialData)
     basic.pause(2000)
     basic.clearScreen()
 }
 let SerialData = ""
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 serial.redirectToUSB()
 basic.showIcon(IconNames.Yes)
 basic.clearScreen()
 basic.forever(function () {
     if (Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P5) < 10) {
         pins.digitalWritePin(DigitalPin.P0, 1)
+        strip.showBarGraph(0, 255)
+        basic.pause(2000)
+        strip.clear()
     } else {
         pins.digitalWritePin(DigitalPin.P0, 0)
+        strip.showBarGraph(0, 255)
+        basic.pause(2000)
+        strip.clear()
     }
 })
