ef699637171ae687b38ff765a74142e1ffb4c6c0
imaonenub <93632222+imaonenub@users.noreply.github.com>
2023-11-02 21:17:30 +0700

    added

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-/**
- * github.com/imaonenub/ai-serial-connection
- */
+function clear () {
+    basic.showString("screen cleaning")
+    basic.pause(100)
+    basic.clearScreen()
+    strip.clear()
+    OLED.clear()
+}
 /**
  * after 2s da code will run
  */
@@ -33,27 +37,35 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     }
 })
 function show_title () {
+    basic.showString("loading..")
+    basic.pause(500)
     basic.showString(SerialData)
-    basic.pause(2000)
-    basic.showString("Ai Trash Bin")
     strip.showColor(neopixel.colors(NeoPixelColors.Green))
     strip.show()
     basic.pause(500)
-    basic.clearScreen()
-    strip.clear()
+    clear()
 }
+/**
+ * github.com/imaonenub/ai-serial-connection
+ */
 let SerialData = ""
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 serial.redirectToUSB()
+basic.showString("TenKeed Bin Starting")
 basic.showIcon(IconNames.Yes)
-basic.clearScreen()
+basic.pause(1000)
+strip.showColor(neopixel.colors(NeoPixelColors.White))
+clear()
 basic.forever(function () {
     if (Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P5) < 10) {
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.showString("opening")
         pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(2000)
-        strip.clear()
+        basic.pause(5000)
+        basic.showString("closing")
         pins.digitalWritePin(DigitalPin.P0, 0)
+        clear()
     }
 })
