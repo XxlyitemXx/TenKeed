c66a598d6feeb95a5f6f42e4190085959904b660
imaonenub <93632222+imaonenub@users.noreply.github.com>
2023-11-02 20:26:01 +0700

    add some thing.. :/

--- main.ts
+++ main.ts
@@ -1,25 +1,33 @@
+/**
+ * github.com/imaonenub/ai-serial-connection
+ */
+/**
+ * after 2s da code will run
+ */
 // This block listens to the website for your class names, and saves them as a variable
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    basic.showString("Your Device Connected")
+    basic.pause(2000)
     SerialData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     // This If statement checks that variable with the class name, and if it matches the class name you entered, it will activate the code within that block
     if (SerialData == "1") {
-        pins.digitalWritePin(DigitalPin.P0, 1)
         show_title()
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(5000)
         pins.digitalWritePin(DigitalPin.P0, 0)
     } else if (SerialData == "2") {
-        pins.digitalWritePin(DigitalPin.P0, 1)
         show_title()
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(5000)
         pins.digitalWritePin(DigitalPin.P0, 0)
     } else if (SerialData == "3") {
-        pins.digitalWritePin(DigitalPin.P0, 1)
         show_title()
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(5000)
         pins.digitalWritePin(DigitalPin.P0, 0)
     } else if (SerialData == "4") {
-        pins.digitalWritePin(DigitalPin.P0, 1)
         show_title()
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(5000)
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
@@ -27,23 +35,25 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
 function show_title () {
     basic.showString(SerialData)
     basic.pause(2000)
+    basic.showString("Ai Trash Bin")
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    basic.pause(500)
     basic.clearScreen()
+    strip.clear()
 }
 let SerialData = ""
-let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 serial.redirectToUSB()
 basic.showIcon(IconNames.Yes)
 basic.clearScreen()
 basic.forever(function () {
     if (Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P5) < 10) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
         pins.digitalWritePin(DigitalPin.P0, 1)
-        strip.showBarGraph(0, 255)
         basic.pause(2000)
         strip.clear()
-    } else {
         pins.digitalWritePin(DigitalPin.P0, 0)
-        strip.showBarGraph(0, 255)
-        basic.pause(2000)
-        strip.clear()
     }
 })
