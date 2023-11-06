b430c1cb4edeaa55f255e885b81b05c776369b58
hellnaw140747 <149694132+hellnaw140747@users.noreply.github.com>
2023-11-06 16:28:17 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 function clear () {
     basic.pause(100)
     basic.clearScreen()
-    strip.clear()
 }
 // This block listens to the website for your class names, and saves them as a variable
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
@@ -9,45 +8,40 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     if (SerialData == "biowaste") {
         show_title()
         servos.P0.setAngle(90)
-        basic.pause(5000)
+        basic.pause(2000)
         servos.P0.setAngle(40)
     } else if (SerialData == "recycle") {
         show_title()
         servos.P1.setAngle(90)
-        basic.pause(5000)
+        basic.pause(2000)
         servos.P1.setAngle(40)
     } else if (SerialData == "dangerous") {
         show_title()
         servos.P2.setAngle(90)
-        basic.pause(5000)
+        basic.pause(2000)
         servos.P2.setAngle(40)
     } else if (SerialData == "normalwaste") {
         show_title()
         pins.digitalWritePin(DigitalPin.P4, 1)
-        basic.pause(5000)
+        basic.pause(2000)
         pins.digitalWritePin(DigitalPin.P4, 0)
     }
 })
 function show_title () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    strip.show()
     basic.pause(100)
     clear()
 }
-/**
- * github.com/imaonenub/ai-serial-connection
- */
 let SerialData = ""
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P5, 30, NeoPixelMode.RGB)
-serial.redirectToUSB()
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate9600
+)
 servos.P0.setAngle(40)
 servos.P1.setAngle(40)
 servos.P2.setAngle(40)
 basic.forever(function () {
     if (grove.measureInInchesV2(DigitalPin.P0) > 1) {
-        strip.showColor(neopixel.colors(NeoPixelColors.White))
-        strip.show()
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(5000)
         pins.digitalWritePin(DigitalPin.P0, 0)
