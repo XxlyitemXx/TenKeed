39b0a435751469aa58dcb01506618d1ac9b4caea
imaonenub <93632222+imaonenub@users.noreply.github.com>
2023-11-06 20:16:30 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
+def show_title():
+    basic.pause(100)
 # This block listens to the website for your class names, and saves them as a variable
 
 def on_data_received():
     global SerialData
     SerialData = serial.read_until(serial.delimiters(Delimiters.NEW_LINE))
-    # This If statement checks that variable with the class name, and if it matches the class name you entered, it will activate the code within that block
-    if SerialData == "1":
-        pins.digital_write_pin(DigitalPin.P0, 1)
-        basic.show_string("1")
-        basic.pause(5000)
-        pins.digital_write_pin(DigitalPin.P0, 0)
-    elif SerialData == "2":
-        pins.digital_write_pin(DigitalPin.P0, 1)
+    if SerialData == "biowaste":
+        show_title()
+        servos.P0.set_angle(90)
+        basic.pause(2000)
+        servos.P0.set_angle(40)
+    elif SerialData == "recycle":
+        show_title()
+        servos.P1.set_angle(90)
+        basic.pause(2000)
+        servos.P1.set_angle(40)
+    elif SerialData == "dangerous":
+        show_title()
         servos.P2.set_angle(90)
-        basic.show_string("2")
-        basic.pause(5000)
-        servos.P2.set_angle(180)
-        pins.digital_write_pin(DigitalPin.P0, 0)
-    elif SerialData == "3":
-        pins.digital_write_pin(DigitalPin.P0, 1)
-        servos.P2.set_angle(90)
-        basic.show_string("3")
-        basic.pause(5000)
-        servos.P2.set_angle(180)
-        pins.digital_write_pin(DigitalPin.P0, 0)
+        basic.pause(2000)
+        servos.P2.set_angle(40)
+    elif SerialData == "normalwaste":
+        show_title()
+        pins.digital_write_pin(DigitalPin.P3, 1)
+        basic.pause(2000)
+        pins.digital_write_pin(DigitalPin.P3, 0)
 serial.on_data_received(serial.delimiters(Delimiters.NEW_LINE), on_data_received)
 
 SerialData = ""
-serial.redirect_to_usb()
-basic.show_icon(IconNames.YES)
-basic.clear_screen()
+serial.redirect(SerialPin.USB_TX, SerialPin.USB_RX, BaudRate.BAUD_RATE9600)
+servos.P0.set_angle(40)
+servos.P1.set_angle(40)
+servos.P2.set_angle(40)
 
 def on_forever():
-    if Environment.sonarbit_distance(Environment.Distance_Unit.DISTANCE_UNIT_CM, DigitalPin.P5) < 10:
+    if grove.measure_in_inches_v2(DigitalPin.P0) > 1:
         pins.digital_write_pin(DigitalPin.P0, 1)
-    else:
+        basic.pause(5000)
         pins.digital_write_pin(DigitalPin.P0, 0)
 basic.forever(on_forever)
-
-def on_forever2():
-    basic.show_string(serial.read_string())
-    basic.pause(2000)
-    basic.clear_screen()
-basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,37 +1,47 @@
-function clear () {
-    basic.pause(100)
-    basic.clearScreen()
-}
 // This block listens to the website for your class names, and saves them as a variable
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     SerialData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     if (SerialData == "biowaste") {
-        show_title()
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        pins.digitalWritePin(DigitalPin.P5, 1)
         servos.P0.setAngle(90)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         basic.pause(2000)
         servos.P0.setAngle(40)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+        strip.clear()
     } else if (SerialData == "recycle") {
-        show_title()
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        pins.digitalWritePin(DigitalPin.P5, 1)
         servos.P1.setAngle(90)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         basic.pause(2000)
         servos.P1.setAngle(40)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+        strip.clear()
     } else if (SerialData == "dangerous") {
-        show_title()
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        pins.digitalWritePin(DigitalPin.P5, 1)
         servos.P2.setAngle(90)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         basic.pause(2000)
         servos.P2.setAngle(40)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+        strip.clear()
     } else if (SerialData == "normalwaste") {
-        show_title()
-        pins.digitalWritePin(DigitalPin.P4, 1)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        pins.digitalWritePin(DigitalPin.P5, 1)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         basic.pause(2000)
-        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+        strip.clear()
     }
 })
-function show_title () {
-    basic.pause(100)
-    clear()
-}
 let SerialData = ""
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P6, 24, NeoPixelMode.RGB)
 serial.redirect(
 SerialPin.USB_TX,
 SerialPin.USB_RX,
@@ -40,11 +50,5 @@ BaudRate.BaudRate9600
 servos.P0.setAngle(40)
 servos.P1.setAngle(40)
 servos.P2.setAngle(40)
-basic.forever(function () {
-    if (grove.measureInInchesV2(DigitalPin.P0) > 1) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        clear()
-    }
-})
+music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+strip.showColor(neopixel.colors(NeoPixelColors.White))
